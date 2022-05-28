7445efe4c0f357ccafbadd2d444550e6323a72f3
romanstetsyk <25715951+romanstetsyk@users.noreply.github.com>
2022-05-28 01:37:10 +0100

    Add button to randomize ship placement

--- public/main.js
+++ public/main.js
@@ -1,153 +1,161 @@
-document.querySelector('#clickMe').addEventListener('click', makeReq)
+// document.querySelector('#clickMe').addEventListener('click', makeReq)
 
-async function makeReq(){
-  const userName = document.querySelector("#userName").value;
-  const res = await fetch(`/api?student=${userName}`)
+// async function makeReq(){
+//   const userName = document.querySelector("#userName").value;
+//   const res = await fetch(`/api?student=${userName}`)
+//   const data = await res.json()
+
+//   console.log(data);
+//   document.querySelector("#playerResult").textContent = data.playerResult
+//   document.querySelector("#computerResult").textContent = data.computerResult
+//   document.querySelector("#gameOutcome").textContent = data.gameOutcome
+// }
+
+
+document.querySelector('#new-board').addEventListener('click', randomize)
+
+async function randomize() {
+  // Sends the request with newBoard query parameter. Expected response is a JSON {height:x, width:y}
+  const res = await fetch(`/api?newBoard=${true}`)
   const data = await res.json()
-
   console.log(data);
-  document.querySelector("#playerResult").textContent = data.playerResult
-  document.querySelector("#computerResult").textContent = data.computerResult
-  document.querySelector("#gameOutcome").textContent = data.gameOutcome
-}
 
+  // Create DOM elements with the board 
+  let playerBoard = document.querySelector('#player-board');
 
-// Each board cell is of the form 'i-j'
-class Board {
-  constructor(h, w) {
-    this.height = h;
-    this.width = w;
-    this.allCells = [];
-    this.blockedCells = [];
-    this.ships = [];
+  // Heading of the board 
+  let heading = document.createElement('section');
+  heading.setAttribute('class', 'column');
+  heading.setAttribute('id', 'rowNumber');
+  let head = document.createElement('div');
+  heading.appendChild(head);
+  for (let i = 0; i < Number(data.height); i += 1) {
+    let div = document.createElement('div');
+    div.textContent = i + 1;
+    heading.appendChild(div);
   }
-  // Sets the list of all cells and available cells. The size of the array is h*w
-  initBoardSize() {
-    for (let i = 0; i < this.width; i += 1) {
-      for (let j = 0; j < this.height; j += 1) {
-        this.allCells.push(`${i}-${j}`);
-        // this.availableCells.push(`${i}-${j}`);
+
+  // replace existing player's board by new one
+  playerBoard.innerHTML = '';
+  playerBoard.appendChild(heading);
+  // create all board cells
+  for (let i = 0; i < Number(data.height); i += 1) {
+    let row = document.createElement('section');
+    row.setAttribute('class', 'column');
+    playerBoard.appendChild(row);
+    let head = document.createElement('div');
+    head.textContent = String.fromCharCode(65 + i); // 65 is a code for capital A
+    row.appendChild(head);
+    for (let j = 0; j < Number(data.width); j += 1) {
+      let div = document.createElement('div');
+      // div.textContent = j + 1;
+      div.setAttribute('data-coord', `${i}-${j}`);
+      if (data.ships.flat().includes(`${i}-${j}`)) {
+        div.setAttribute('class', 'my-ship');
       }
+      row.appendChild(div);
     }
-  }
-
-  randomBoard(ArrayOfShipSizes) {
-    this.initBoardSize();
-    ArrayOfShipSizes.map(e => this.placeShip(e));
-  }
-
-  placeShip(shipSize) {
-    // Returns the random number between 0 and n inclusive
-    // rand(1) means a random number 0 or 1
-    const rand = n => Math.floor(Math.random() * (n + 1));
-
-    // Choose the direction randomly. 1 - horizontal, 0 - vertical
-    let direction = rand(1) ? 'horizontal' : 'vertical';
     
-    const shipCells = [];
-    let availCells = [];
-    let blockedCells = [];
-
-    // for horizontal cells the adjacent cells are 1 away, for vertical - the width of the board
-    const step = (direction === 'horizontal') ? 1 : this.width;
-    // helper variables to check if subsequent horizontal and vertical elements are available
-    const [x,y] = (direction === 'horizontal') ? [0,1] : [1,0]
-
-    if (direction === 'horizontal') {
-      // Check if there's enough space to put the ship of size 'shipSize' horizontally
-      for (let k = 0; k < this.allCells.length; k += 1) {
-        let availLength = 0;
-        let diff = 0;
-        for (let l = k; l < k + shipSize * step; l += step) {
-          let elem = this.allCells[k].split('-').map(Number);
-          let nextElem = this.allCells[l]?.split('-').map(Number);
-          if (elem[0] === nextElem?.[0] && elem[1] === nextElem?.[1] - diff && !this.blockedCells.includes(this.allCells[l])) {
-            availLength += 1;
-          } else {
-            availLength = 0;
-            blockedCells.push(this.allCells[k]);
-            break;
-          }
-          diff += 1;
-        }
-        if (availLength === shipSize) {
-          availCells.push(this.allCells[k]);
-        }
-        // console.log(this.allCells[k].split('-').map(Number), availLength);
-      }
-      // Choose ramdomly a cell from available cells to start building a ship
-      const startingCell = availCells[rand(availCells.length - 1)];
-      const startingCellIndex = this.allCells.indexOf(startingCell);
-
-      for (let i = startingCellIndex; i < startingCellIndex + shipSize; i += 1) {
-        // update cells for current ship
-        shipCells.push(this.allCells[i]);
-        // Block adjacent cells
-        [
-          this.allCells[i],
-          this.allCells[i - 1]?.split('-')[0] === this.allCells[i]?.split('-')[0] ? this.allCells[i - 1] : '', // check if row of the next elem in the same line
-          this.allCells[i + 1]?.split('-')[0] === this.allCells[i]?.split('-')[0] ? this.allCells[i + 1] : '',
-          this.allCells[i - this.width],
-          this.allCells[i + this.width]
-        ].forEach(e => {
-          if (!this.blockedCells.includes(e)) {
-            this.blockedCells.push(e);
-          }
-        })
-      }
-      this.ships.push(shipCells);
-      return 
-    }
-    else if (direction === 'vertical') {
-      // Check if there's enough space to put the ship of size 'shipSize' horizontally
-      for (let k = 0; k < this.allCells.length; k += 1) {
-        let availLength = 0;
-        let diff = 0;
-        for (let l = k; l < k + shipSize * this.width; l += this.width) {
-          let elem = this.allCells[k].split('-').map(Number);
-          let nextElem = this.allCells[l]?.split('-').map(Number);
-          if (elem[1] === nextElem?.[1] && elem[0] === nextElem?.[0] - diff && !this.blockedCells.includes(this.allCells[l])) {
-            availLength += 1;
-          } else {
-            availLength = 0;
-            blockedCells.push(this.allCells[k]);
-            break;
-          }
-          diff += 1;
-        }
-        if (availLength === shipSize) {
-          availCells.push(this.allCells[k]);
-        }
-        // console.log(this.allCells[k].split('-').map(Number), availLength);
-      }
-
-      // Choose ramdomly a cell from available cells to start building a ship
-      const startingCell = availCells[rand(availCells.length - 1)];
-      const startingCellIndex = this.allCells.indexOf(startingCell);
-
-      for (let i = startingCellIndex; i < startingCellIndex + shipSize * this.width; i += this.width) {
-        shipCells.push(this.allCells[i]);
-        // Block adjacent cells
-        [
-          this.allCells[i],
-          this.allCells[i - 1]?.split('-')[0] === this.allCells[i]?.split('-')[0] ? this.allCells[i - 1] : '', // check if row of the next elem in the same line
-          this.allCells[i + 1]?.split('-')[0] === this.allCells[i]?.split('-')[0] ? this.allCells[i + 1] : '',
-          this.allCells[i - this.width],
-          this.allCells[i + this.width]
-        ].forEach(e => {
-          if (!this.blockedCells.includes(e)) {
-            this.blockedCells.push(e);
-          }
-        })
-      }
-      this.ships.push(shipCells);
-      return
-    }
   }
-
 }
-let board = new Board(10, 10);
-board.initBoardSize();
-board.placeShip(5)
-console.log(board.ships)
-console.log(board.blockedCells);
+
+
+
+
+
+
+
+
+// class Board {
+//   constructor(h, w) {
+//     this.height = h;
+//     this.width = w;
+//     this.allCells = [];
+//     this.blockedCells = [];
+//     this.ships = [];
+//   }
+//   // Sets the list of all cells and available cells. The size of the array is h*w
+//   initBoardSize() {
+//     let player = document.querySelector('#player-board');
+//     for (let i = 0; i < this.width; i += 1) {
+//       for (let j = 0; j < this.height; j += 1) {
+//         this.allCells.push(`${i}-${j}`);
+
+//       }
+//     }
+//   }
+
+//   randomBoard(ArrayOfShipSizes) {
+//     this.initBoardSize();
+//     ArrayOfShipSizes.map(e => this.placeShip(e));
+//   }
+
+//   placeShip(shipSize) {
+//     // Returns the random number between 0 and n inclusive
+//     // rand(1) means a random number 0 or 1
+//     const rand = n => Math.floor(Math.random() * (n + 1));
+
+//     // Choose the direction randomly. 1 - horizontal, 0 - vertical
+//     let direction = rand(1) ? 'horizontal' : 'vertical';
+    
+//     const shipCells = [];
+//     let availCells = [];
+//     let blockedCells = [];
+
+//     // for horizontal cells the adjacent cells are 1 away, for vertical - the width of the board
+//     const step = (direction === 'horizontal') ? 1 : this.width;
+//     // helper variables to check if subsequent horizontal and vertical elements are available
+//     const [x,y] = (direction === 'horizontal') ? [0,1] : [1,0]
+
+//     // Check if there's enough space to put the ship of size 'shipSize' horizontally
+//     for (let k = 0; k < this.allCells.length; k += 1) {
+//       let availLength = 0;
+//       let diff = 0;
+//       for (let l = k; l < k + shipSize * step; l += step) {
+//         let elem = this.allCells[k].split('-').map(Number);
+//         let nextElem = this.allCells[l]?.split('-').map(Number);
+//         if (elem[x] === nextElem?.[x] && elem[y] === nextElem?.[y] - diff && !this.blockedCells.includes(this.allCells[l])) {
+//           availLength += 1;
+//         } else {
+//           availLength = 0;
+//           blockedCells.push(this.allCells[k]);
+//           break;
+//         }
+//         diff += 1;
+//       }
+//       if (availLength === shipSize) {
+//         availCells.push(this.allCells[k]);
+//       }
+//       // console.log(this.allCells[k].split('-').map(Number), availLength);
+//     }
+//     // Choose ramdomly a cell from available cells to start building a ship
+//     const startingCell = availCells[rand(availCells.length - 1)];
+//     const startingCellIndex = this.allCells.indexOf(startingCell);
+
+//     for (let i = startingCellIndex; i < startingCellIndex + shipSize * step; i += step) {
+//       // update cells for current ship
+//       shipCells.push(this.allCells[i]);
+//       // Block adjacent cells
+//       [
+//         this.allCells[i],
+//         this.allCells[i - 1]?.split('-')[0] === this.allCells[i]?.split('-')[0] ? this.allCells[i - 1] : '', // check if row of the next elem in the same line
+//         this.allCells[i + 1]?.split('-')[0] === this.allCells[i]?.split('-')[0] ? this.allCells[i + 1] : '',
+//         this.allCells[i - this.width],
+//         this.allCells[i + this.width]
+//       ].forEach(e => {
+//         if (!this.blockedCells.includes(e)) {
+//           this.blockedCells.push(e);
+//         }
+//       })
+//     }
+//     this.ships.push(shipCells);
+//     return
+
+//   }
+
+// }
+// let board = new Board(10, 10);
+// board.initBoardSize();
+// board.placeShip(5)
+// console.log(board.ships)
+// console.log(board.blockedCells);
--- server.js
+++ server.js
@@ -3,6 +3,114 @@ const fs = require('fs')
 const url = require('url');
 const querystring = require('querystring');
 const figlet = require('figlet')
+// const express = require("express")
+
+// // installed express module and created a constant app in order to host this website publically via glitch on port 3000
+// const app = express()
+// app.use(express.json())
+// app.use(express.static("public"))
+
+
+// After instantiating call .initBoardSize(height, width) method to set the height, width, and allCells properties.
+// allCells is an array where each element is 'x-y'. x and y coordinates are separated by dashes. It's easy to parse them later like this using .split('-').
+// The main method here is .randomBoard(array). All other are helper methods. I'm not sure how to hide them.
+// In .randomBoard(array) the array is the sizes of ships. For example [5,4,3,3] means 1 ship of 5 squares, 1 ship of 4 squares, and 2 ships of 3 squares.
+// They are randomly placed in such way that only corners can touch.
+// .ships property returns the array of ships (occupied squares)
+// .blockedCells property is a helper property, that should be hidden too.. maybe later
+class Board {
+  constructor(h, w) {
+    this.height = h;
+    this.width = w;
+    this.allCells = [];
+    this.blockedCells = [];
+    this.ships = [];
+  }
+  // Sets the list of all cells and available cells. The size of the array is h*w
+  initBoardSize() {
+    for (let i = 0; i < this.height; i += 1) {
+      for (let j = 0; j < this.width; j += 1) {
+        this.allCells.push(`${i}-${j}`);
+      }
+    }
+  }
+
+  randomBoard(ArrayOfShipSizes) {
+    this.allCells = [];
+    this.blockedCells = [];
+    this.ships = [];
+    this.initBoardSize();
+    ArrayOfShipSizes.map(e => this.placeShip(e));
+  }
+
+  placeShip(shipSize) {
+    // Returns the random number between 0 and n inclusive
+    // rand(1) means a random number 0 or 1
+    const rand = n => Math.floor(Math.random() * (n + 1));
+
+    // Choose the direction randomly. 1 - horizontal, 0 - vertical
+    let direction = rand(1) ? 'horizontal' : 'vertical';
+    
+    const shipCells = [];
+    let availCells = [];
+    let blockedCells = [];
+
+    // for horizontal cells the adjacent cells are 1 away, for vertical - the width of the board
+    const step = (direction === 'horizontal') ? 1 : this.width;
+    // helper variables to check if subsequent horizontal and vertical elements are available
+    const [x,y] = (direction === 'horizontal') ? [0,1] : [1,0]
+
+    // Check if there's enough space to put the ship of size 'shipSize' horizontally
+    for (let k = 0; k < this.allCells.length; k += 1) {
+      let availLength = 0;
+      let diff = 0;
+      for (let l = k; l < k + shipSize * step; l += step) {
+        let elem = this.allCells[k].split('-').map(Number);
+        let nextElem = this.allCells[l]?.split('-').map(Number);
+        if (elem[x] === nextElem?.[x] && elem[y] === nextElem?.[y] - diff && !this.blockedCells.includes(this.allCells[l])) {
+          availLength += 1;
+        } else {
+          availLength = 0;
+          blockedCells.push(this.allCells[k]);
+          break;
+        }
+        diff += 1;
+      }
+      if (availLength === shipSize) {
+        availCells.push(this.allCells[k]);
+      }
+      // console.log(this.allCells[k].split('-').map(Number), availLength);
+    }
+    // Choose ramdomly a cell from available cells to start building a ship
+    const startingCell = availCells[rand(availCells.length - 1)];
+    const startingCellIndex = this.allCells.indexOf(startingCell);
+
+    for (let i = startingCellIndex; i < startingCellIndex + shipSize * step; i += step) {
+      // update cells for current ship
+      shipCells.push(this.allCells[i]);
+      // Block adjacent cells
+      [
+        this.allCells[i],
+        this.allCells[i - 1]?.split('-')[0] === this.allCells[i]?.split('-')[0] ? this.allCells[i - 1] : '', // check if row of the next elem in the same line
+        this.allCells[i + 1]?.split('-')[0] === this.allCells[i]?.split('-')[0] ? this.allCells[i + 1] : '',
+        this.allCells[i - this.width],
+        this.allCells[i + this.width]
+      ].forEach(e => {
+        if (!this.blockedCells.includes(e)) {
+          this.blockedCells.push(e);
+        }
+      })
+    }
+    this.ships.push(shipCells);
+    return
+  }
+}
+
+// initialize player and computer boards globally so that they are available in all routes.
+// maybe we should put it inside createServer function
+let player;
+let computer;
+
 
 const server = http.createServer((req, res) => {
   const page = url.parse(req.url).pathname;
@@ -15,8 +123,35 @@ const server = http.createServer((req, res) => {
       res.end();
     });
   }
+  // added a instructions page below to confirm everything is still linking with using express hosting
+  else if (page == '/instructions') {
+    fs.readFile('instructions.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
   
   else if (page == '/api') {
+    console.log('request to api went through');
+    if('newBoard' in params) {
+
+      player = new Board(10,10);
+      player.randomBoard([5,4,3,3,2]);
+
+      computer = new Board(5, 5);
+      computer.randomBoard([5,4,3,3,2]);
+
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      const objToJson = {
+        height: player.height,
+        width: player.width,
+        ships: player.ships
+      }
+      res.end(JSON.stringify(objToJson));
+    }
+
+
     if('student' in params){
       if(params['student']== 'leon'){
         res.writeHead(200, {'Content-Type': 'application/json'});
@@ -61,5 +196,5 @@ const server = http.createServer((req, res) => {
     });
   }
 });
-
-server.listen(8000);
+// server is hosted and listening on a provided port OR port 3000
+server.listen(process.env.PORT || 3000);
